8624518be50803a8e94864bc9c0563e253e3a0a5
Doug Red <douglasred63@yahoo.com>
2022-12-15 17:11:59 -0600

    added dynamic question rendering and page  redirection

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,35 +2,36 @@ const startQuizEl = document.querySelector(".start-quiz");
 const promptBoxEl = document.querySelector(".prompt-box");
 const promptTextEl = document.querySelector(".prompt");
 const mainTextEl = document.querySelector(".main-text");
+const buttonContainerEl = document.querySelector(".button-container");
 let shuffledQuestions;
 let currentQuestionIndex;
-
+let score = 0;
 const questions = [
   {
     question: "This is Question 1",
     answers: [
       { text: "Answer choice 1", correct: true },
-      { text: "Answer choice 2", correct: false },
-      { text: "Answer choice 3", correct: false },
-      { text: "Answer choice 4", correct: false },
+      { text: "tfyrfjjyyhjy", correct: false },
+      { text: "Ayjyyyjgfhhjnice 3", correct: false },
+      { text: "Cdfhjmmjukjk,j", correct: false },
     ],
   },
   {
     question: "This is Question 2",
     answers: [
-      { text: "Answer choice 1", correct: false },
-      { text: "Answer choice 2", correct: true },
-      { text: "Answer choice 3", correct: false },
-      { text: "Answer choice 4", correct: false },
+      { text: "Aghfjhjhjfj", correct: false },
+      { text: "fhfgkuyttdg", correct: true },
+      { text: "323546667587", correct: false },
+      { text: "087sfgjhgfjf87", correct: false },
     ],
   },
   {
     question: "This is Question 3",
     answers: [
-      { text: "Answer choice 1", correct: false },
-      { text: "Answer choice 2", correct: false },
-      { text: "Answer choice 3", correct: true },
-      { text: "Answer choice 4", correct: false },
+      { text: "07894jh54kl5", correct: false },
+      { text: "3544uktyuk", correct: false },
+      { text: "4tythgfdsghkj", correct: true },
+      { text: "Happy Birthday", correct: false },
     ],
   },
 ];
@@ -40,39 +41,63 @@ const startQuiz = function () {
   mainTextEl.className = "hidden";
   shuffledQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
-  console.log(shuffledQuestions);
   setNextQuestion();
 };
 
 const setNextQuestion = function () {
+  resetState();
   displayQuestion(shuffledQuestions[currentQuestionIndex]);
 };
 
 const displayQuestion = function (newQuestion) {
-  let question = questions[currentQuestionIndex];
+  if (currentQuestionIndex === questions.length) {
+    scoreBoard();
+  }
   promptTextEl.textContent = newQuestion.question;
   newQuestion.answers.forEach((answer) => {
     const button = document.createElement("button");
     button.textContent = answer.text;
     button.className = "btn answer-choice";
-    console.log(button);
-    promptBoxEl.appendChild(button);
+    buttonContainerEl.appendChild(button);
 
     if (answer.correct) {
-      button.setAttribute("correct", true);
+      button.setAttribute("correct", true, "quiz-btn", true);
     } else {
-      button.setAttribute("incorrect", true);
+      button.setAttribute("incorrect", true), "quiz-btn", true;
     }
 
     button.addEventListener("click", selectAnswer);
   });
-  console.log(newQuestion, question);
+  currentQuestionIndex++;
 };
 
 const selectAnswer = function (event) {
   let target = event.target;
   console.log(target);
   console.log(target.hasAttribute("correct"));
+  if (target.hasAttribute("correct")) {
+    score += 20;
+    console.log("Correct answer! +20 points");
+    console.log(score);
+    setNextQuestion();
+  } else {
+    score -= 15;
+    if (score < 0) {
+      score = 0;
+    }
+    console.log(score);
+    console.log("Incorrect answer! -15 points");
+    setNextQuestion();
+  }
+};
+
+const resetState = function () {
+  promptTextEl.innerHTML = "";
+  buttonContainerEl.innerHTML = "";
+};
+
+const scoreBoard = function () {
+  window.location.assign("./scoreboard.html");
 };
 
 startQuizEl.addEventListener("click", startQuiz);
